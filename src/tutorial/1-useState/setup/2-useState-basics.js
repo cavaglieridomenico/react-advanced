aca8b3f6b0e860ae9cda44254b5eb8e5b429c318
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-09-16 14:23:43 +0200

    [ADDED] useState basic example

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("Random Text");
+  const handleClick = () => {
+    text === "Random Text" ? setText("Hello!") : setText("Random Text");
+  };
+  return (
+    <div className="container">
+      <h2>{text}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Text
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
