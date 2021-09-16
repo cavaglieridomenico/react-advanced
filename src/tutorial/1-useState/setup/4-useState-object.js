f12834af22f3eb1c6b5f400925d6be614a8b4cee
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-09-16 19:16:37 +0200

    [ADDED] useState-Object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+const user = {
+  name: "Max",
+  surname: "Cavalera",
+  message: "Hello!",
+};
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState(user);
+
+  const handleClick = () => {
+    setPerson({ ...person, message: "Goodnight!" });
+  };
+
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.surname}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={handleClick}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
