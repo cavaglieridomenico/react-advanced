489c4bedb638fe08e6ddcfe5e3001fcc84856741
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-09-18 19:45:34 +0200

    [ADDED] useEffect basics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Setup from "./tutorial/1-useState/setup/3-useState-array";
+import React from 'react'
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics'
 function App() {
   return (
-    <div className="container">
+    <div className='container'>
       <Setup />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
