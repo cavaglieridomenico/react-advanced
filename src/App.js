4bd01e0511ae9f6c82c0421e3396b65c37d51912
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-09-21 20:05:17 +0200

    [ADDED] Show-Hide Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
+import React from 'react';
+import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
 function App() {
   return (
     <div className='container'>
       <Setup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <h2>show/hide</h2>
+      <button className='btn' onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {show && <Item />}
+    </>
+  );
+};
+
+const Item = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const newWidth = () => {
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener('resize', newWidth);
+    return () => {
+      window.removeEventListener('resize', newWidth);
+    };
+  }, []);
+  return (
+    <div style={{ marginTop: '40px' }}>
+      <h3>Window</h3>
+      <p>Size: {width} px</p>
+    </div>
+  );
 };
 
 export default ShowHide;
