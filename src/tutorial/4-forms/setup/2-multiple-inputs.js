d9f2cc5165cceb8c345d071e45edd40a2e097245
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-09-23 13:56:54 +0200

    [ADDED] Form with multiple inputs
    Created a Component that contains a form with inputs controlled
    simultaneously by a single useState () and a single function with an
    onChange listener. The handleSubmit function is invoked by an onClick
    listener on the button instead of onSubmit on the form.

--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -7,36 +7,48 @@ import React, { useState } from 'react';
 // dynamic object keys
 
 const ControlledInputs = () => {
-  const [firstName, setFirstName] = useState('');
-  const [email, setEmail] = useState('');
+  const [person, setPerson] = useState({
+    firstName: '',
+    email: '',
+    age: '',
+  });
   const [people, setPeople] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (firstName && email) {
-      const person = { id: new Date().getTime().toString(), firstName, email };
-      console.log(person);
-      setPeople((people) => {
-        return [...people, person];
-      });
-      setFirstName('');
-      setEmail('');
+  const handleChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setPerson(currentPerson => ({ ...currentPerson, [name]: value }));
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (person.firstName && person.email && person.age) {
+      const newPerson = { ...person, id: new Date().getTime().toString() };
+      setPeople(currentPeople => [...currentPeople, newPerson]);
+      setPerson(currentPerson => ({
+        ...currentPerson,
+        id: '',
+        firstName: '',
+        email: '',
+        age: '',
+      }));
     } else {
-      console.log('empty values');
+      console.log('No empty string, please');
     }
   };
+
   return (
     <>
       <article>
-        <form className='form' onSubmit={handleSubmit}>
+        <form className='form'>
           <div className='form-control'>
             <label htmlFor='firstName'>Name : </label>
             <input
               type='text'
               id='firstName'
               name='firstName'
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+              value={person.firstName}
+              onChange={handleChange}
             />
           </div>
           <div className='form-control'>
@@ -45,17 +57,30 @@ const ControlledInputs = () => {
               type='email'
               id='email'
               name='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={person.email}
+              onChange={handleChange}
             />
           </div>
-          <button type='submit'>add person</button>
+          <div className='form-control'>
+            <label htmlFor='email'>Age : </label>
+            <input
+              type='age'
+              id='age'
+              name='age'
+              value={person.age}
+              onChange={handleChange}
+            />
+          </div>
+          <button type='submit' onClick={handleSubmit}>
+            add person
+          </button>
         </form>
         {people.map((person, index) => {
-          const { id, firstName, email } = person;
+          const { id, firstName, age, email } = person;
           return (
             <div className='item' key={id}>
               <h4>{firstName}</h4>
+              <h5>{age}</h5>
               <p>{email}</p>
             </div>
           );
